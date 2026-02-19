500503651734776a1304285295850335a47fd1cf
Xougui <xougui.7@gmail.com>
2026-02-19 17:36:40 +0100

    css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,45 +2,51 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Lyxios Docs',
-			
-			// ✅ C'est Starlight qui prend le contrôle exclusif des langues
-			defaultLocale: 'root',
-			locales: {
-				root: {
-					label: 'Français',
-					lang: 'fr',
-				},
-				en: {
-					label: 'English',
-					lang: 'en',
-				},
-				es: {
-					label: 'Español',
-					lang: 'es',
-				}
-			},
+  integrations: [
+      starlight({
+          title: 'Lyxios Docs',
+          
+          // ✅ C'est Starlight qui prend le contrôle exclusif des langues
+          defaultLocale: 'root',
+          locales: {
+              root: {
+                  label: 'Français',
+                  lang: 'fr',
+              },
+              en: {
+                  label: 'English',
+                  lang: 'en',
+              },
+              es: {
+                  label: 'Español',
+                  lang: 'es',
+              }
+          },
 
-			social: [{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/R5Zs9m2u7X' }],
-			sidebar: [
-				{ label: 'Accueil', link: '/' },
-				{
-					label: 'Démarrage',
-					autogenerate: { directory: 'guides/demarrage' },
-				},
-				{
-					label: 'Modules',
-					autogenerate: { directory: 'guides/modules' },
-				},
-				{
-					label: 'Commandes',
-					autogenerate: { directory: 'commandes' },
-				},
-			],
-		}),
+          social: [{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/R5Zs9m2u7X' }],
+          sidebar: [
+              { label: 'Accueil', link: '/' },
+              {
+                  label: 'Démarrage',
+                  autogenerate: { directory: 'guides/demarrage' },
+              },
+              {
+                  label: 'Modules',
+                  autogenerate: { directory: 'guides/modules' },
+              },
+              {
+                  label: 'Commandes',
+                  autogenerate: { directory: 'commandes' },
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
